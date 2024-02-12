3a76aafb187000e48008f89ac95f3666a8d9a4c6
Mikalai Rusakovich <kolyos83@mail.ru>
2024-02-12 15:36:23 +0300

    feat: add interfaces

--- src/interfaces/data.interface.ts
+++ src/interfaces/data.interface.ts
@@ -1,6 +1,16 @@
 export interface IUser {
-    id: string;
-    name: string;
-    age: number;
-    hobbies: string[];
+  id: string;
+  name: string;
+  age: number;
+  hobbies: string[];
 }
+
+export interface IData {
+  users: IUser[];
+}
+
+export interface IError {
+  error: string;
+}
+
+export type ApiResponse = IUser[] | IUser | IError | null;
--- src/server.ts
+++ src/server.ts
@@ -1,61 +1,50 @@
-import {createServer, ServerResponse, IncomingMessage} from 'http';
-import {v4 as uuidv4} from 'uuid';
-import {IUser} from "./interfaces/data.interface";
+import { createServer, ServerResponse, IncomingMessage } from 'http';
+import { v4 as uuidv4 } from 'uuid';
+import { ApiResponse, IUser } from './interfaces/data.interface';
+import { data } from './Users/users';
 
-// Массив для хранения данных
-let users: IUser[] = [{id: "1234-1234-1234-1234", name: "Nick", age: 35, hobbies: ['box']}];
-
-// Функция для отправки ответа клиенту
-function sendResponse(res: ServerResponse, statusCode: number, body: any) {
-    res.writeHead(statusCode, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify(body));
+function sendResponse(
+  res: ServerResponse,
+  statusCode: number,
+  body: ApiResponse
+) {
+  res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(body));
 }
 
-// Создание HTTP-сервера
-export const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-    const {method, url} = req;
+export const server = createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    const { method, url } = req;
 
-    // Обработчик GET-запроса для получения всех данных
     if (method === 'GET' && url === '/api/users') {
-        sendResponse(res, 200, users);
+      sendResponse(res, 200, data.users);
+    } else if (method === 'POST' && url === '/api/users') {
+      let body = '';
+      req.on('data', chunk => {
+        body += chunk.toString();
+      });
+      req.on('end', () => {
+        const newUser: IUser = { id: uuidv4(), ...JSON.parse(body) };
+        data.users.push(newUser);
+        sendResponse(res, 201, newUser);
+      });
+    } else if (method === 'PUT' && url?.startsWith('/api/users/')) {
+      const id = url.slice(10); // Удаляем "/api/data/" из URL
+      let body = '';
+      req.on('data', chunk => {
+        body += chunk.toString();
+      });
+      req.on('end', () => {
+        const newUser: IUser = { ...JSON.parse(body), id };
+        data.users = data.users.map(item => (item.id === id ? newUser : item));
+        sendResponse(res, 200, newUser);
+      });
+    } else if (method === 'DELETE' && url?.startsWith('/api/users/')) {
+      const id = url.slice(10); // Удаляем "/api/data/" из URL
+      data.users = data.users.filter(item => item.id !== id);
+      sendResponse(res, 204, null);
+    } else {
+      sendResponse(res, 404, { error: 'Not Found' });
     }
-
-    // Обработчик POST-запроса для создания новых данных
-    else if (method === 'POST' && url === '/api/users') {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        req.on('end', () => {
-            const newUser: IUser = {id: uuidv4(), ...JSON.parse(body)};
-            users.push(newUser);
-            sendResponse(res, 201, newUser);
-        });
-    }
-
-    // Обработчик PUT-запроса для обновления данных по идентификатору
-    else if (method === 'PUT' && url?.startsWith('/api/users/')) {
-        const id = url.slice(10); // Удаляем "/api/data/" из URL
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        req.on('end', () => {
-            const newUser: IUser = {...JSON.parse(body), id};
-            users = users.map(item => (item.id === id ? newUser : item));
-            sendResponse(res, 200, newUser);
-        });
-    }
-
-    // Обработчик DELETE-запроса для удаления данных по идентификатору
-    else if (method === 'DELETE' && url?.startsWith('/api/users/')) {
-        const id = url.slice(10); // Удаляем "/api/data/" из URL
-        users = users.filter(item => item.id !== id);
-        sendResponse(res, 204, null);
-    }
-
-    // Обработчик ошибки для неизвестных запросов
-    else {
-        sendResponse(res, 404, {error: 'Not Found'});
-    }
-});
+  }
+);
