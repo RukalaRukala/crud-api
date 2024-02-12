a6ea53e555b803235f5d5f8b8c9bfdbc8b7072a4
Mikalai Rusakovich <kolyos83@mail.ru>
2024-02-13 02:21:34 +0300

    feat: implement horizontal scaling

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import { server } from './server/server';
 import dotenv from 'dotenv';
+import cluster from 'cluster';
+import os from 'os';
 
 dotenv.config();
 
-const PORT = process.env.PORT;
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const PORT = parseInt(process.env.PORT || '8000');
+const numCPUs = os.cpus().length;
+
+if (cluster.isPrimary) {
+  for (let i = 1; i < numCPUs; i++) {
+    cluster.fork({ PORT: `${PORT + i}` });
+  }
+
+  cluster.on('exit', worker => {
+    console.log(`Worker ${worker.process.pid} died`);
+    cluster.fork({ PORT: `${PORT + worker.id}` });
+  });
+} else {
+  server.listen(process.env.PORT, () => {
+    console.log(
+      `Worker ${cluster.worker?.id} is running on http://localhost:${process.env.PORT}`
+    );
+  });
+}
